9c8ef38233210f11c8fa003d4fd268421a9ed26e
予纾 <100036558+Yuisyuu@users.noreply.github.com>
2024-05-09 10:10:14 +0800

    fix: Adjust messages

--- plugins/maimai/maimaiDX/GenB50.py
+++ plugins/maimai/maimaiDX/GenB50.py
@@ -564,7 +564,7 @@ async def generateb50(b35: list, b15: list, nickname: str, qq, dani: int, type: 
     ttf = ImageFont.truetype(ttf_bold_path, size=14)
     ImageDraw.Draw(b50).text(
         (196, 148),
-        f'过往版本: {b35_ra} + 现行版本: {b15_ra} = {rating}',
+        f'过往版本：{b35_ra} + 现行版本：{b15_ra} = {rating}',
         font=ttf,
         fill=(255, 255, 255),
     )
