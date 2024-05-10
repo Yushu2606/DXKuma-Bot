1da7908e0cf78c49dd00f3100a019449d0770631
FangChanguwu <1716530046@qq.com>
2024-05-10 18:42:14 +0800

    feat: Add exception logger impl

---
+++ plugins/bot/except_log/__init__.py
@@ -0,0 +1,15 @@
+import traceback
+from nonebot import get_bot
+from nonebot.adapters.onebot.v11 import MessageSegment
+from nonebot.message import run_postprocessor
+
+
+@run_postprocessor
+async def _(event, matcher, exception):
+    if not exception:
+        return
+    bot = get_bot()
+    msg = MessageSegment.text(
+        f"检测到未捕获的异常：\n{str(traceback.format_exception(exception))}"
+    )
+    await bot.send_msg(group_id=236030263, message=msg)
