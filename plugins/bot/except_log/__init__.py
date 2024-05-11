09ae9f81a54219d89f5e90cb5cf34ca638e0fb7b
予纾 <100036558+Yuisyuu@users.noreply.github.com>
2024-05-11 10:53:47 +0800

    feat: Improve except log

--- plugins/bot/except_log/__init__.py
+++ plugins/bot/except_log/__init__.py
@@ -1,15 +1,17 @@
 import traceback
+from typing import Optional
 
 from nonebot import get_bot
-from nonebot.adapters.onebot.v11 import MessageSegment
+from nonebot.adapters.onebot.v11 import MessageSegment, Event
+from nonebot.internal.matcher import Matcher
 from nonebot.message import run_postprocessor
 
 
 @run_postprocessor
-async def _(event, matcher, exception):
+async def _(event: Event, matcher: Matcher, exception: Optional[Exception]):
     if not exception:
         return
     bot = get_bot()
     trace = "".join(traceback.format_exception(exception)).replace("\\n", "\n")
-    msg = MessageSegment.text(trace)
+    msg = MessageSegment.text(f"{trace}\n{event.raw_message}")
     await bot.send_msg(group_id=236030263, message=msg)
