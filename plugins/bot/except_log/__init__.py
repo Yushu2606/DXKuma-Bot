4444e32c70903702e30e6156d8626d59cf33e5f0
予纾 <100036558+Yuisyuu@users.noreply.github.com>
2024-05-12 10:38:05 +0800

    fix: Fix reporting of except log

--- plugins/bot/except_log/__init__.py
+++ plugins/bot/except_log/__init__.py
@@ -10,11 +10,16 @@ from nonebot.message import run_postprocessor
 
 @run_postprocessor
 async def _(event: Event, matcher: Matcher, exception: Optional[Exception]):
-    if not exception or (isinstance(exception, ActionFailed) and exception.info["retcode"] == 200):
+    if not exception or (
+        isinstance(exception, ActionFailed) and exception.info["retcode"] == 200
+    ):
         return
     bot = get_bot()
     trace = "".join(traceback.format_exception(exception)).replace("\\n", "\n")
     msg = MessageSegment.text(f"{trace}\n{event.raw_message}")
     await bot.send_msg(group_id=236030263, message=msg)
-    feedback = MessageSegment.text("迪拉熊出了点问题呢x")
+    feedback = (
+        MessageSegment.reply(event.message_id),
+        MessageSegment.text("迪拉熊出了点问题呢x"),
+    )
     await matcher.send(feedback)
